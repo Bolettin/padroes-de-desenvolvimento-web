7cffcf8cac0b8d513cd8d069b34c181212b8930c
Matheus Boletti <125603677+Bolettin@users.noreply.github.com>
2026-08-26 01:36:44 +0000

    configuracao das entidades

---
+++ ecommerce/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO categoria (nome,descricao) VALUES ('Informatica', 'Produtos de informatica');
+
+INSERT INTO categoria (nome,descricao) VALUES ('Livros', 'Livros técnicos');
+
+INSERT INTO categoria (nome,descricao) VALUES ('Eletrônicos', 'Aparelhos Eletrônicos');
+
